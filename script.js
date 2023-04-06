d221d0d9deef1610a9e8cbbae1b34fa3e9a83261
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-04-06 16:08:44 +0530

    Wrap the dom manipulation methods in its factory

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
+/* eslint-disable wrap-iife */
 /* eslint-disable no-plusplus */
-const main = document.querySelector('.main');
-function createHeading() {
-  const headingDiv = document.createElement('div');
-  const headingPara = document.createElement('p');
-  headingDiv.setAttribute('class', 'heading');
-  headingPara.textContent = 'Tic-Tac-Toe';
-  headingDiv.appendChild(headingPara);
-  main.appendChild(headingDiv);
-}
 
-function createBoard() {
-  const boardDiv = document.createElement('div');
-  for (let i = 0; i < 9; i++) {
-    const cellBtn = document.createElement('button');
-    cellBtn.setAttribute('class', `cell${i}`);
-    boardDiv.appendChild(cellBtn);
+const gameBoard = (() => {
+  const main = document.querySelector('.main');
+  function displayHeading() {
+    const headingDiv = document.createElement('div');
+    const headingPara = document.createElement('p');
+    headingDiv.setAttribute('class', 'heading');
+    headingPara.textContent = 'Tic-Tac-Toe';
+    headingDiv.appendChild(headingPara);
+    main.appendChild(headingDiv);
   }
-  boardDiv.setAttribute('class', 'board');
-  main.appendChild(boardDiv);
-}
-function displayResult() {
-  const resultDiv = document.createElement('div');
-  resultDiv.setAttribute('class', 'result');
-  const resultPara = document.createElement('p');
-  resultPara.textContent = 'Player X turn';
-  resultDiv.appendChild(resultPara);
-  main.appendChild(resultDiv);
-}
-createHeading();
-displayResult();
-createBoard();
+  function displayResult() {
+    const resultDiv = document.createElement('div');
+    resultDiv.setAttribute('class', 'result');
+    const resultPara = document.createElement('p');
+    resultPara.textContent = 'Player X turn';
+    resultDiv.appendChild(resultPara);
+    main.appendChild(resultDiv);
+  }
+  function displayContent() {
+    const contentDiv = document.createElement('div');
+    contentDiv.setAttribute('class', 'content');
+    main.appendChild(contentDiv);
+  }
+  function displayBoard() {
+    const content = document.querySelector('.content');
+    const boardDiv = document.createElement('div');
+    for (let i = 0; i < 9; i++) {
+      const cellBtn = document.createElement('button');
+      cellBtn.setAttribute('class', `cell${i}`);
+      boardDiv.appendChild(cellBtn);
+    }
+    boardDiv.setAttribute('class', 'board');
+    content.appendChild(boardDiv);
+  }
+
+  return {
+    displayHeading,
+    displayContent,
+    displayBoard,
+    displayResult,
+  };
+})();
+
+const Game = (() => {
+  const createLayout = () => {
+    gameBoard.displayHeading();
+    gameBoard.displayResult();
+    gameBoard.displayContent();
+    gameBoard.displayBoard();
+  };
+
+  return {
+    createLayout,
+  };
+})();
+Game.createLayout();
